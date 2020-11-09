6d0eb2ae4eb1d1f0ac05cdb86a7c857f2b03bc3b
Tyrel Jonas Mora <tyreljonas.mora@gmail.com>
2020-11-09 11:15:33 +0800

    more code for AbstractWord

--- src/AbstractWord.ts
+++ src/AbstractWord.ts
@@ -1,4 +1,4 @@
-import {inflexpPattern} from "./patterns"
+import {inflexpPattern, rightwardRepetitionPattern, syllableRepeatPattern} from "./patterns"
 import Syllable from "./Syllable"
 
 
@@ -24,7 +24,66 @@ export default abstract class AbstractWord {
 
     static _repeat (word: AbstractWord, groups: {[key:string]: string}) {
         if (groups.rightwardRepetitionFirst !== undefined) {
-            
+            let i = -1;
+            [...(groups.rightwardRepetitionFirst + groups.rightwardRepetitionRest).split(":")].forEach((subinflexp) => {
+                i++
+                let j = 0
+                let r: Syllable[] = [new Syllable()]
+                let subgroups = subinflexp.match(rightwardRepetitionPattern)!.groups!
+                let placeAfter = subgroups.placeAfter !== undefined ? 1 : 0
+                if (subgroups.specialMarkBefore !== undefined)
+                    r[j].premark = "-"
+                if (subgroups.magnetBefore !== undefined) {
+                    [...subgroups.magnetBefore.split("~")].slice(1).forEach((special) => {
+                        try {
+                            switch (special) {
+                                case "":
+                                    let popped = word.value[i - 1 + placeAfter].coda.pop()
+                                    if (popped !== undefined)
+                                        r[j].onset.push(popped)
+                                    break
+                                case "$":
+                                    r[j].stress = word.value[i - 1 + placeAfter].stress
+                                    word.value[i - 1 + placeAfter].stress = 0
+                                    break
+                                case "%":
+                                    r[j].vowelLength = word.value[i - 1 + placeAfter].vowelLength
+                                    word.value[i - 1 + placeAfter].vowelLength = 8
+                                    break
+                                case "@":
+                                    r[j].tone = word.value[i - 1 + placeAfter].tone
+                                    word.value[i - 1 + placeAfter].tone = 0
+                                    break
+                                default:
+                            }
+                        }
+                        catch (e) {
+                            throw new Error("Inflexp Magnet Error: ~" + special + " (from " + subgroups.magnetBefore + ") failed to take sounds from the previous syllable.")
+                        }
+                    })
+                }
+                [...(subgroups.first+subgroups.rest).split(",")].forEach((s) => {
+                    if (j > 0)
+                        r.push(new Syllable())
+                    let subsubgroups = s.match(syllableRepeatPattern)!.groups!;
+                    [...(subsubgroups.specialsBefore + subsubgroups.specialsAfter)].forEach((ch, k) => {
+                        if (k % 2 === 1)
+                            return
+                        switch (ch) {
+                            case "$":
+                                r[j].stress = word.value[i+j].stress
+                                break
+                            case "$":
+                                r[j].vowelLength = word.value[i+j].vowelLength
+                                break
+                            case "@":
+                                r[j].tone = word.value[i+j].tone
+                                break
+                        }
+                    })
+                    j++
+                })
+            })
         }
         else if (groups.rightwardRepetitionFirst !== undefined) {
 
