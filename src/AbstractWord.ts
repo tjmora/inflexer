8237906f6ff3665458e4a129a91f32b9de66cc04
Tyrel Jonas Mora <tyreljonas.mora@gmail.com>
2020-11-10 10:17:35 +0800

    completed rightwardRepetition process

--- src/AbstractWord.ts
+++ src/AbstractWord.ts
@@ -25,16 +25,16 @@ export default abstract class AbstractWord {
     static _repeat (word: AbstractWord, groups: {[key:string]: string}) {
         if (groups.rightwardRepetitionFirst !== undefined) {
             let i = -1;
-            [...(groups.rightwardRepetitionFirst + groups.rightwardRepetitionRest).split(":")].forEach((subinflexp) => {
+            (groups.rightwardRepetitionFirst + groups.rightwardRepetitionRest).split(":").forEach((subinflexp) => {
                 i++
                 let j = 0
                 let r: Syllable[] = [new Syllable()]
                 let subgroups = subinflexp.match(rightwardRepetitionPattern)!.groups!
-                let placeAfter = subgroups.placeAfter !== undefined ? 1 : 0
-                if (subgroups.specialMarkBefore !== undefined)
+                let placeAfter = subgroups.placeAfter !== "" ? 1 : 0
+                if (subgroups.specialMarkBefore !== "")
                     r[j].premark = "-"
-                if (subgroups.magnetBefore !== undefined) {
-                    [...subgroups.magnetBefore.split("~")].slice(1).forEach((special) => {
+                if (subgroups.magnetBefore !== "") {
+                    subgroups.magnetBefore.split("~").slice(1).forEach((special) => {
                         try {
                             switch (special) {
                                 case "":
@@ -62,11 +62,11 @@ export default abstract class AbstractWord {
                         }
                     })
                 }
-                [...(subgroups.first+subgroups.rest).split(",")].forEach((s) => {
+                (subgroups.first+subgroups.rest).split(",").forEach((s) => {
                     if (j > 0)
                         r.push(new Syllable())
                     let subsubgroups = s.match(syllableRepeatPattern)!.groups!;
-                    [...(subsubgroups.specialsBefore + subsubgroups.specialsAfter)].forEach((ch, k) => {
+                    (subsubgroups.specialsBefore + subsubgroups.specialsAfter).split("").forEach((ch, k) => {
                         if (k % 2 === 1)
                             return
                         switch (ch) {
@@ -81,12 +81,89 @@ export default abstract class AbstractWord {
                                 break
                         }
                     })
+                    if (subsubgroups.main !== "") {
+                        let temp: string | undefined = ""
+                        if (subsubgroups.main.search("1") > -1 || subsubgroups.main === "*" || subsubgroups.main === "**") {
+                            r[j].onset = word.value[i+j].onset.map(ch => ch)
+                        }
+                        else if (subsubgroups.main.search("2") > -1) {
+                            temp = word.value[i+j].onset.shift()
+                            if (temp !== undefined)
+                                r[j].onset.push(temp)
+                        }
+                        else if (subsubgroups.main.search("3") > -1) {
+                            temp = word.value[i+j].onset.pop()
+                            if (temp !== undefined)
+                                r[j].onset.push(temp)
+                        }
+                        if (subsubgroups.main.search("4") > -1 || subsubgroups.main.includes("*")) {
+                            r[j].nucleus = word.value[i+j].nucleus.map(ch => ch)
+                        }
+                        else if (subsubgroups.main.search("5") > -1) {
+                            temp = word.value[i+j].nucleus.shift()
+                            if (temp !== undefined)
+                                r[j].onset.push(temp)
+                        }
+                        else if (subsubgroups.main.search("6") > -1) {
+                            temp = word.value[i+j].nucleus.pop()
+                            if (temp !== undefined)
+                                r[j].onset.push(temp)
+                        }
+                        if (subsubgroups.main.search("7") > -1 || subsubgroups.main === "**" || subsubgroups.main === "***") {
+                            r[j].coda = word.value[i+j].coda.map(ch => ch)
+                        }
+                        else if (subsubgroups.main.search("8") > -1) {
+                            temp = word.value[i+j].coda.shift()
+                            if (temp !== undefined)
+                                r[j].onset.push(temp)
+                        }
+                        else if (subsubgroups.main.search("9") > -1) {
+                            temp = word.value[i+j].coda.pop()
+                            if (temp !== undefined)
+                                r[j].onset.push(temp)
+                        }
+                    }
+                    if (subsubgroups.duplicator !== "")
+                        r.splice(r.length, 0, ...r.map(syll => syll.copy()))
                     j++
                 })
+                if (subgroups.magnetAfter !== "") {
+                    subgroups.magnetAfter.split("~").slice(1).forEach((special) => {
+                        try {
+                            switch (special) {
+                                case "":
+                                    let popped = word.value[i + placeAfter].coda.pop()
+                                    if (popped !== undefined)
+                                        r[j].onset.push(popped)
+                                    break
+                                case "$":
+                                    r[j].stress = word.value[i + placeAfter].stress
+                                    word.value[i - 1 + placeAfter].stress = 0
+                                    break
+                                case "%":
+                                    r[j].vowelLength = word.value[i + placeAfter].vowelLength
+                                    word.value[i - 1 + placeAfter].vowelLength = 8
+                                    break
+                                case "@":
+                                    r[j].tone = word.value[i + placeAfter].tone
+                                    word.value[i - 1 + placeAfter].tone = 0
+                                    break
+                                default:
+                            }
+                        }
+                        catch (e) {
+                            throw new Error("Inflexp Magnet Error: ~" + special + " (from " + subgroups.magnetAfter + ") failed to take sounds from the next syllable.")
+                        }
+                    })
+                }
+                if (subgroups.specialMarkAfter !== "")
+                    r[j].postmark = "-"
+                if (r.length > 0)
+                    word.value.splice(i + placeAfter, 0, ...r)
             })
         }
-        else if (groups.rightwardRepetitionFirst !== undefined) {
-
+        else if (groups.leftwardRepetitionFirst !== undefined) {
+            
         }
         else if (groups.baseRepetition !== undefined) {
 
