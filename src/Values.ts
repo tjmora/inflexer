46b87c880924ae7787d7df78641b8b080eab69f9
Tyrel Jonas Mora <tyreljonas.mora@gmail.com>
2020-12-20 23:39:10 +0800

    some edits and added npm publish workflow

---
+++ src/Values.ts
@@ -0,0 +1,9 @@
+export let InflexerValues = {
+    noStress: 0,
+    secondaryStress: 2,
+    primaryStress: 4,
+    extraShortVowel: 2,
+    shortVowel: 4,
+    normalLengthVowel: 8,
+    longVowel: 12
+}
